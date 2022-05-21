9912c82c639e59c33e2db3ad46321e71575aee80
MrT <turgeon.dev@gmail.com>
2022-05-20 22:55:41 -0600

    Added: contactitems const

---
+++ client/consts/contactItems.js
@@ -0,0 +1,20 @@
+// icons
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+
+const contactItems = [
+  {
+    title: "Email",
+    icon: MdEmail
+  },
+  {
+    title: "LinkedIn",
+    icon: BsLinkedin
+  },
+  {
+    title: "Github",
+    icon: BsGithub
+  },
+]
+
+export default contactItems
