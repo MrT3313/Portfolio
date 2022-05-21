b5dca30759b8271642c3d7a21a74150088226814
MrT <turgeon.dev@gmail.com>
2022-05-20 22:54:30 -0600

    Added STRUCTURAL: IconWrapper

---
+++ client/components/structural/IconWrapper/index.js
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled'
+import { Text } from '../Text/styled'
+const IconWrapper = styled(Text)`
+  display: flex;
+`
+
+export default IconWrapper
--- client/components/structural/index.js
+++ client/components/structural/index.js
@@ -1,2 +1,3 @@
 export { default as Flex } from './Flex'
 export { default as Text } from './Text'
+export { default as IconWrapper } from './IconWrapper'
