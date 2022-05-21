9d67200706493174f41e0b77d0d54bd5e3e97c48
MrT <turgeon.dev@gmail.com>
2022-05-21 15:30:09 -0600

    Created a new component that renders in an rc-tooltip when the user hovers over the email icon in the contact me component
    the user can rightClick to copy the email or leftClick to open their default email editor

---
+++ client/components/view/prompts/email/index.js
@@ -0,0 +1,45 @@
+// components
+import { Flex, Text } from "../../../structural";
+import Image from "next/image";
+// styles
+
+const EmailPrompt = () => {
+  return (
+    <Flex>
+      <Flex 
+        direction="column" 
+        justify="center" align="center" 
+        width="70px"
+      >
+        <Image
+          src="/assets/icons/icons8-leftClick-52.svg" 
+          width={50} height={50}
+        />
+        <Text 
+          as="div" 
+          margin="10px 0 0 0"
+        >
+          Send Email
+        </Text>
+      </Flex>
+      <Flex 
+        direction="column" 
+        justify="center" align="center" 
+        width="70px"
+      >
+        <Image
+          src="/assets/icons/icons8-rightClick-50.svg" 
+          width={50} height={50}
+        />
+        <Text 
+          as="div" 
+          margin="10px 0 0 0"
+        >
+          Copy Email
+        </Text>
+      </Flex>
+    </Flex>
+  )
+}
+
+export default EmailPrompt
---
+++ client/components/view/prompts/index.js
@@ -0,0 +1,1 @@
+export { default as EmailPrompt} from './email'
