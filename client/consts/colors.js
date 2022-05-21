ce555f6f7fa04f0db6aaaac977492dcfde96fe5c
MrT <turgeon.dev@gmail.com>
2022-05-20 19:18:21 -0600

    Added: initial colors & theme consts

---
+++ client/consts/colors.js
@@ -0,0 +1,7 @@
+const colors = {
+  RED: '#FF0000',
+  GREEN: '#4cbb17',
+  YELLOW: '#FFFF00',
+}
+
+export default colors
---
+++ client/consts/theme.js
@@ -0,0 +1,9 @@
+import colors from './colors'
+
+const theme = {
+  PRIMARY: colors.RED,
+  SECONDARY: colors.GREEN,
+  TERTIARY: colors.YELLOW,
+}
+
+export default theme
