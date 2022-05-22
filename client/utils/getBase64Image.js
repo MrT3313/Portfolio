314f1871cb53285e59315981c2deb21ab2a76a61
MrT <turgeon.dev@gmail.com>
2022-05-21 21:06:13 -0600

    Added Util: getBase64Image

---
+++ client/utils/getBase64Image.js
@@ -0,0 +1,14 @@
+const getBase64Image = async (url) => {
+  const data = await fetch(url);
+  const blob = await data.blob();
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob); 
+    reader.onloadend = () => {
+      const base64data = reader.result;   
+      resolve(base64data);
+    }
+  });
+}
+
+export default getBase64Image
