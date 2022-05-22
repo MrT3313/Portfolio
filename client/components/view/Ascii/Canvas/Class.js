85c1a835fc7a535f41ab6083f45fa6af164fb130
MrT <turgeon.dev@gmail.com>
2022-05-21 21:06:56 -0600

    ascii working for fixed image -- random images is NOT working
    look into potentially changin height and width properties on the image

--- client/components/view/Ascii/Canvas/Class.js
+++ client/components/view/Ascii/Canvas/Class.js
@@ -8,8 +8,10 @@ export class Cell {
     this.color = color
   }
   draw (context) { 
+    context.lineWidth = 2
     context.fillStyle = this.color;
     context.fillText(this.symbol, this.x, this.y);
+    context.stroke()
   }
 }
 
@@ -24,31 +26,21 @@ export class CanvasClass {
     this.height = height;
     this.context.drawImage(image, 0, 0, this.width, this.height);
     this.pixels = this.context.getImageData(0, 0, this.width, this.height);
+    console.log('LOOK THE PIXELS', this.pixels)
 
 
     this.history = []
   }
 
   convertToSymbol = (averageColorValue) => {
-    console.log('THE AVERAGE COLOR VALUE', averageColorValue)
     const percent = parseFloat(averageColorValue / this.maxRgb).toFixed(2)
-    console.log('THE PERCENT', percent)
-    
     const targetChar = Math.round(percent * this.characterRamp.length)
-    console.log('THE TARGET CHAR', targetChar)
-
-    // const buckets = round(this.maxRgb / this.characterRamp.length)
-    // console.log('THE BUCKETS', buckets)
-
-    console.log('characterRamp', this.characterRamp.length, this.characterRamp)
     const result = this.characterRamp.charAt(targetChar - 1)
-    console.log('THE THING TO RETURN', result)
 
     this.history.push({
       avg: averageColorValue,
       res: result
     })
-
     return result
   }
   scanImage = (cellSize) => {
@@ -66,14 +58,14 @@ export class CanvasClass {
           const total = red + green + blue;
           const averageColorValue = total / 3;
   
-          const color = `rgb("${red},${green},${blue}")`;
+          const color = `rgb(${red},${green},${blue})`;
+          console.log('THE COLOR', color)
           const symbol = this.convertToSymbol(averageColorValue);
   
           if (total < 700) this.imageCellArray.push(new Cell(x, y, symbol, color));
         }
       }
     }
-    console.log('CHECK -- imageCellArray', this.imageCellArray)
   }
   drawAscii = () => {
     this.context.clearRect(0, 0, this.width, this.height)
