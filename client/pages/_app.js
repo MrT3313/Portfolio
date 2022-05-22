dbe678e143433a336e13626752c3d8f79e9b3443
MrT <turgeon.dev@gmail.com>
2022-05-22 15:36:17 -0600

    Rendering w/ empty React Query

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 
+// rQuery
+import { QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from "react-query/devtools";
+import { queryClient } from '../rQuery'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
