690dc78676fc1c7ed9decbe41f9a4e5f3a108052
MrT <turgeon.dev@gmail.com>
2022-05-22 18:52:37 -0600

    Added: upsplash

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['picsum.photos']
+    domains: ['picsum.photos', 'images.unsplash.com']
+  },
+  env: {
+    UPSPLASH_ACCESS_KEY: process.env.UPSPLASH_ACCESS_KEY,
+    UPSPLASH_SECRET_KEY: process.env.UPSPLASH_SECRET_KEY,
   }
 }
 
