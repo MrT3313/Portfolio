4a08eda5abca03b09e423774220c6c297ca4137a
MrT <turgeon.dev@gmail.com>
2022-05-29 17:57:30 -0600

    Installed & Updated: eslint

---
+++ client/.eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'object-curly-spacing': ['error', 'always'],
+		'max-len': ['error', {
+			code: 100,
+			ignoreStrings: true,
+			ignoreUrls: true,
+		}],
+		'react/react-in-jsx-scope': 'off',
+		// allow console.log during development only
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		// disable prop validation
+		'react/prop-types': 0
+	}
+};
